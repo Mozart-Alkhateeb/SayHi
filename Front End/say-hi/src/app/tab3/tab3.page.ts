fa8643a9a9aa97f90f16c968a160f1fd1cd07b48
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2020-07-12 14:37:17 +0300

    Created Basic layout for matches screen Scaffold Android project

--- Front End/say-hi/src/app/tab3/tab3.page.ts
+++ Front End/say-hi/src/app/tab3/tab3.page.ts
@@ -3,10 +3,61 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  matches = [
+    {
+      name: 'Yara',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Maya',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Warda',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Linda',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Jessi',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Mike',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Jack',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Jasmine',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Mary',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+    {
+      name: 'Dora',
+      avatar:
+        'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+    },
+  ];
 
   constructor() {}
-
 }
--- Front End/say-hi/src/app/tabs/tabs.page.ts
+++ Front End/say-hi/src/app/tabs/tabs.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  @ViewChild('ionTabs', { static: true }) ionTabs: IonTabs;
+  selectedTab = 'tab1';
+
+  tabs = [
+    {
+      tab: 'tab1',
+      icon: 'person-circle-sharp',
+      label: 'Account',
+    },
+    {
+      tab: 'tab2',
+      icon: 'heart-circle-sharp',
+      label: 'Favorites',
+    },
+    {
+      tab: 'tab3',
+      icon: 'chatbubbles-sharp',
+      label: 'Matches',
+    },
+  ];
 
   constructor() {}
 
+  layout(e) {
+    console.log(e);
+    return 'icon-top';
+  }
+
+  ionTabsDidChange() {
+    this.selectedTab = this.ionTabs.getSelected();
+    console.log(this.ionTabs.getSelected());
+  }
 }
